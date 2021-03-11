f3fe69361c8b9dbb1fa547b8e22e319f6a098473
Memo Khoury <majd.khoury93@gmail.com>
2021-03-11 12:44:06 -0500

    feat: finalize contract

--- contracts/InventorCampaign.sol
+++ contracts/InventorCampaign.sol
@@ -1,6 +1,15 @@
 pragma solidity ^0.4.17;
 
 contract InventorCampaign {
+
+    // the owner of this contract and the respective
+    // crowdfunding campaign
+    address public manager;
+    uint public minimumContribution;
+    mapping(address=>bool) public approvers;
+    Request[] public requests;
+    uint public approversCount;
+    
     struct Request {
         // why is this request being created?
         string description;
@@ -10,15 +19,11 @@ contract InventorCampaign {
         address recipient;
         // is the request complete
         bool complete;
+        // keep track on whether someone has voted
+        mapping(address=>bool) approvals;
+        // numbner of approvals we got for a payment createRequest
+        uint approvalCount;
     }
-    // the owner of this contract and the respective
-    // crowdfunding campaign
-    address public manager;
-    uint public minimumContribution;
-    address[] public approvers;
-    Request[] public requests;
-
-
     function InventorCampaign(uint _minimumContribution) public {
         manager = msg.sender;
         minimumContribution = _minimumContribution;
@@ -27,11 +32,17 @@ contract InventorCampaign {
     // contribute functuon can run into a little issue
     function contribute() public payable {
         require(msg.value > minimumContribution);
-        approvers.push(msg.sender);
+        approvers[msg.sender] = true;
+        approversCount++;
     }
 
     modifier restricted() {
         require(msg.sender == manager);
+        _;
+    }
+    
+    modifier isApprover() {
+        require(approvers[msg.sender] == true);
         _;
     }
 
@@ -40,8 +51,32 @@ contract InventorCampaign {
             description: _description,
             value: _value,
             recipient: _recipient,
-            complete: false
+            complete: false,
+            approvalCount: 0
         });
         requests.push(newRequest);
+    }
+    
+        
+    function approveRequest(uint requestIndex) public isApprover {
+        // Store this instance of request as a storage variable so 
+        // that we can override it!
+        Request storage request = requests[requestIndex];
+        
+        // Has this user not voted before? If they voted before
+        // then don't allow them to vote. If not, allow them to
+        // vote.
+        require(request.approvals[msg.sender] == false);
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+    
+    function finalizeRequest(uint requestIndex) public restricted {
+        Request storage request = requests[requestIndex];
+        // first, we check if the request has NOT been completed
+        require(request.approvalCount >  (approversCount / 2));
+        require(request.complete == false);
+        request.recipient.transfer(request.value);
+        request.complete = true;
     }
 }
