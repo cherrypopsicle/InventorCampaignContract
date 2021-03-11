621146b990d01a145079e16f158e75c580c38cbc
Memo Khoury <majd.khoury93@gmail.com>
2021-03-11 07:37:20 -0500

    feat: createRequest()

--- contracts/InventorCampaign.sol
+++ contracts/InventorCampaign.sol
@@ -10,24 +10,38 @@ contract InventorCampaign {
         address recipient;
         // is the request complete
         bool complete;
-        /// ???
-        bool[] vote;
     }
     // the owner of this contract and the respective
     // crowdfunding campaign
     address public manager;
-    uint public minimunContribution;
+    uint public minimumContribution;
     address[] public approvers;
+    Request[] public requests;
 
 
     function InventorCampaign(uint _minimumContribution) public {
         manager = msg.sender;
-        minimunContribution = _minimumContribution;
+        minimumContribution = _minimumContribution;
     }
 
     // contribute functuon can run into a little issue
     function contribute() public payable {
         require(msg.value > minimumContribution);
         approvers.push(msg.sender);
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function createRequest(string _description, uint _value, address _recipient) public restricted {
+        Request memory newRequest = Request({
+            description: _description,
+            value: _value,
+            recipient: _recipient,
+            complete: false
+        });
+        requests.push(newRequest);
     }
 }
