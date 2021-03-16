85e9088677d567f0a0f1a6f87536762cd9d77a65
Memo Khoury <majd.khoury93@gmail.com>
2021-03-16 15:55:20 -0400

    server and routes.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const { createServer } = require("http");
+const next = require("next");
+
+// checks the NODE_ENV variable to see if it's in production mode
+const app = next({
+  dev: process.env.NODE_ENV !== "production",
+});
+
+// load the routes ..
+const routes = require("./routes");
+// 
+const handler = routes.getRequestHandler(app);
+
+// create server on the port 3000
+app.prepare().then(() => {
+  createServer(handler).listen(3000, (err) => {
+    if (err) throw err;
+    console.log("Listening on localhost:3000");
+  });
+});
